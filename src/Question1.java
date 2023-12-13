259663880f722c6405148f1abe93d44f4ad090de
samridhip <pandeysamridhi1@gmail.com>
2023-12-13 23:09:19 +0530

    enums

---
+++ src/Question1.java
@@ -0,0 +1,14 @@
+public class Question1 {
+    public enum days_of_the_week {
+        SUNDAY,
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,}
+        public static void main(String[] args) {
+        days_of_the_week today=days_of_the_week.MONDAY;
+        System.out.println("today is "+ today);
+        }
+    }

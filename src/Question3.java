2b012ea0873ecc7a009665764401b569eaec0e1a
samridhip <pandeysamridhi1@gmail.com>
2023-12-14 00:05:02 +0530

    enums

--- src/Question3.java
+++ src/Question3.java
@@ -13,5 +13,10 @@ public class Question3 {
         shapes fourth=shapes.TRIANGLE;
         shapes fifth=shapes.RECTANGULAR;
         System.out.println("first shape is "+first);
+        System.out.println("first shape is "+second);
+        System.out.println("first shape is "+third);
+        System.out.println("first shape is "+fourth);
+        System.out.println("first shape is "+fifth);
+
     }
 }

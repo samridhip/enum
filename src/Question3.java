4c0971862cee532a0c9925efb57f4ca4b55b7981
samridhip <pandeysamridhi1@gmail.com>
2023-12-13 23:46:58 +0530

    enums

---
+++ src/Question3.java
@@ -0,0 +1,17 @@
+public class Question3 {
+    public enum shapes{
+        CIRCLE,
+        RECTANGULAR,
+        SQUARE,
+        HEXAGON,
+        TRIANGLE,
+    }
+    public static void main(String[]args){
+        shapes first=shapes.CIRCLE;
+        shapes second=shapes.HEXAGON;
+        shapes third=shapes.SQUARE;
+        shapes fourth=shapes.TRIANGLE;
+        shapes fifth=shapes.RECTANGULAR;
+        System.out.println();
+    }
+}
